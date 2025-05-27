b64a4502d33e17a40cbd63e0dc9ea6b054c21dc9
LUKY12 <108628978+Luisito012244837@users.noreply.github.com>
2025-05-27 08:31:32 -0400

    Delete src directory

--- src/Components/Authentication/Privateroute.jsx
+++
@@ -1,9 +0,0 @@
-const PrivateRoute = ({ element: Element, ...rest }) => {
-  const { isAuthenticated } = useSelector((state) => state.auth);
-  
-  return isAuthenticated ? (
-    <Element {...rest} />
-  ) : (
-    <Navigate to="/login" replace />
-  );
-};
